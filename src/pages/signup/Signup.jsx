8fa449bd2c6c79af76918202580499471ba567a0
Brandon Franks <bgfranks.dev@gmail.com>
2021-11-19 17:29:15 -0500

    created the signup UI

--- src/pages/signup/Signup.jsx
+++ src/pages/signup/Signup.jsx
@@ -1,7 +1,46 @@
+import { useState } from 'react';
+
+// styles
+import './Signup.scss';
+
 export default function Signup() {
+  const [email, setEmail] = useState('');
+  const [displayName, setDisplayName] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(email, password, displayName);
+  };
+
   return (
-    <div>
-      <h2>Signup</h2>
-    </div>
+    <form className='signup-form' onSubmit={handleSubmit}>
+      <h2>Create an Account</h2>
+      <label>
+        <span>email:</span>
+        <input
+          type='email'
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+      </label>
+      <label>
+        <span>display name:</span>
+        <input
+          type='text'
+          value={displayName}
+          onChange={(e) => setDisplayName(e.target.value)}
+        />
+      </label>
+      <label>
+        <span>password:</span>
+        <input
+          type='password'
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+      </label>
+      <button className='btn'>Sign Up</button>
+    </form>
   );
 }
