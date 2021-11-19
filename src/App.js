b85c37099767d4b6eff90467a141ab5bf97f42d1
Brandon Franks <bgfranks.dev@gmail.com>
2021-11-19 17:06:45 -0500

    created the nav ui

--- src/App.js
+++ src/App.js
@@ -4,11 +4,13 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/home/Home';
 import Login from './pages/login/Login';
 import Signup from './pages/signup/Signup';
+import Navbar from './components/navbar/Navbar';
 
 function App() {
   return (
     <div className='App'>
       <Router>
+        <Navbar />
         <Routes>
           <Route exact path='/' element={<Home />} />
           <Route path='/signup' element={<Signup />} />
