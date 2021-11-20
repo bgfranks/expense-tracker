21739bd794372062eafd2aeef534d69fabef4731
Brandon Franks <bgfranks.dev@gmail.com>
2021-11-19 20:36:27 -0500

    added the firebase dependency and initialized firebase, firestore, and firebase auth

---
+++ src/firebase/config.js
@@ -0,0 +1,23 @@
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/auth';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyCyAy6zPWKPt6ji5Bqt9gWtCN-R72babqA',
+  authDomain: 'expensely-c2137.firebaseapp.com',
+  projectId: 'expensely-c2137',
+  storageBucket: 'expensely-c2137.appspot.com',
+  messagingSenderId: '627067668273',
+  appId: '1:627067668273:web:9df2dc21c9e9efd19ed210',
+};
+
+// init firebase
+firebase.initializeApp(firebaseConfig);
+
+// init firestore
+const db = firebase.firestore();
+
+// init firebase auth
+const auth = firebase.auth();
+
+export { db, auth };
