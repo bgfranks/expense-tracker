15fb3e894895971c9a437ed10c3c819dbb18c835
Brandon Franks <bgfranks.dev@gmail.com>
2021-11-21 19:17:25 -0500

    created a log out button on the navbar and added cleanup functions to the signup and logout hooks

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,9 +1,12 @@
 import { Link } from 'react-router-dom';
+import { useLogout } from '../../hooks/useLogout';
 
 // styles
 import './Navbar.scss';
 
 export default function Navbar() {
+  const { logout } = useLogout();
+
   return (
     <div className='navbar'>
       <ul>
@@ -13,6 +16,11 @@ export default function Navbar() {
         </li>
         <li>
           <Link to='/signup'>Sign Up</Link>
+        </li>
+        <li>
+          <button className='btn' onClick={logout}>
+            Log Out
+          </button>
         </li>
       </ul>
     </div>
