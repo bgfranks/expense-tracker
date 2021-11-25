ad1c573865026cf7934577f8f87a4b5b3bbb70aa
Brandon Franks <bgfranks.dev@gmail.com>
2021-11-25 18:10:55 -0500

    created the functionality to delete a transaction

--- src/pages/home/TransactionList.jsx
+++ src/pages/home/TransactionList.jsx
@@ -1,10 +1,15 @@
+import { useFirestore } from '../../hooks/useFirestore';
+
 export default function TransactionList({ transactions }) {
+  const { deleteDocument } = useFirestore('transactions');
+
   return (
     <ul className='transactions'>
       {transactions.map((transaction) => (
         <li key={transaction.id}>
           <p className='name'>{transaction.name}</p>
           <p className='amount'>${transaction.amount}</p>
+          <button onClick={() => deleteDocument(transaction.id)}>x</button>
         </li>
       ))}
     </ul>
